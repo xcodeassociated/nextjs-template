59b766fa82b897e833a3c0f103aef2ebefeb4e3a
xcodeassociated <janusz.majchrzak@protonmail.com>
2024-09-09 22:03:43 +0200

    rename styles directory and add multi-arch docker builds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '@/styles/globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 
 export const metadata: Metadata = {
